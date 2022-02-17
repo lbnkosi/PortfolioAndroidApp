529b9268ba3f0fa51770075034b2ec8cc82defd2
Lebogang Nkosi <lebogangnko@discovery.co.za>
2022-02-17 21:15:00 +0200

    Few changes

--- app/src/main/java/za/co/lbnkosi/portfolio/presentation/features/home/HomeActivity.kt
+++ app/src/main/java/za/co/lbnkosi/portfolio/presentation/features/home/HomeActivity.kt
@@ -2,11 +2,14 @@ package za.co.lbnkosi.portfolio.presentation.features.home
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import dagger.hilt.android.AndroidEntryPoint
 import za.co.lbnkosi.portfolio.R
 import za.co.lbnkosi.portfolio.databinding.ActivityHomeBinding
+import za.co.lbnkosi.portfolio.domain.model.BackStack
 import za.co.lbnkosi.portfolio.presentation.base.BaseActivity
+import za.co.lbnkosi.portfolio.presentation.base.BaseFragment
 
 @AndroidEntryPoint
 class HomeActivity : BaseActivity() {
@@ -18,6 +21,22 @@ class HomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+    }
+
+    private fun showHomeFragment() {
+        binding.backStack = BackStack(isHome = true)
+    }
+
+    private fun showPortfolioFragment() {
+        binding.backStack = BackStack(isPortfolio = true)
+    }
+
+    private fun showAboutFragment() {
+        binding.backStack = BackStack(isAbout = true)
+    }
+
+    private fun showSettingsFragment() {
+        binding.backStack = BackStack(isSettings = true)
     }
 
 }
---
+++ app/src/main/java/za/co/lbnkosi/portfolio/presentation/features/portfolio/PortfolioFragment.kt
@@ -0,0 +1,23 @@
+package za.co.lbnkosi.portfolio.presentation.features.portfolio
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import za.co.lbnkosi.portfolio.databinding.FragmentPortfolioBinding
+import za.co.lbnkosi.portfolio.presentation.base.BaseFragment
+import za.co.lbnkosi.portfolio.presentation.features.home.HomeViewModel
+
+class PortfolioFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentPortfolioBinding
+
+    private val viewModel: HomeViewModel by activityViewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentPortfolioBinding.inflate(inflater)
+        return binding.root
+    }
+
+}
